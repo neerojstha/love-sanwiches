c8237daef6dd29d3ae7c44c8d6aa34aa6e61feb8
neerojstha <nirojstha@hotmail.com>
2022-09-04 10:46:01 +0000

    calculate stock numbers and a dd to stock worksheet

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 from pprint import pprint 
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -15,7 +16,10 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
     """"
-    Get sales figure from the user
+    Get sales figure from the user.
+    Run a while loopp to collect a valid string of data from the user
+    via terminal, which must be a string of 6 numbers separated
+    by commas, the loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
@@ -32,14 +36,15 @@ def get_sales_data():
 
     return sales_data
    
+
 def validate_data(values):
     """
     Inside the try, converts all string values into intergers. 
-    Raises ValueError of strings cannot be converte into int,
+    Raises ValueError of strings cannot be converted into int,
     if there aren't exactely 6 values
     """
-    print(values)
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
@@ -51,14 +56,11 @@ def validate_data(values):
     return True
 
 
-
-
 def update_worksheet(data, worksheet):
     """
     Receives a list of integers to  be inserted into a worksheet
     Update the relevant worksheet with the data provided
     """
-
     print(f"Updating {worksheet} worksheet...\n")
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
@@ -73,7 +75,6 @@ def calculate_surplus_data(sales_row):
     -positive surplus indicate waste
     -Negative surplus indicate extra made when stock was sold out.
     """
-
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
@@ -82,23 +83,40 @@ def calculate_surplus_data(sales_row):
     for stock, sales in zip(stock_row, sales_row):
        surplus = int(stock) - sales
        surplus_data.append(surplus)
+    
     return surplus_data  
 
 
 def get_last_5_entries_sales():
     """
-    collect collums of data from sales worksheet, collecting the last 5 entries
+    collect collums of data from sales worksheet, collecting
+     the last 5 entries
     for each sandwich and returns the data as a  list of lists.
     """
     sales = SHEET.worksheet("sales")
-    # column = sales.col_values(3)
-    # print(column)
-
+   
     columns = []
     for ind in range(1,7):
         column = sales.col_values(ind)
         columns.append(column[-5:])
-    pprint(columns)
+    
+    return columns
+
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
 def main():
     """
     Run all program functions
@@ -108,8 +126,11 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 print("welcome to lovee sandwiches Data Automation") 
-# main()   
+main()   
 
-get_last_5_entries_sales()
+
